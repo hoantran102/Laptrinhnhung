e9ad318af3b46e3ddedda3a0e41d5e93f4ab17ac
hoantran102 <hoanbem1234@gmail.com>
2021-09-25 13:35:04 +0700

    Update cam_bien_ND.ino

--- cam bien nhiet do/cam_bien_ND.ino
+++ cam bien nhiet do/cam_bien_ND.ino
@@ -1,18 +1,23 @@
-int sensorPin = A0;// chân analog kết nối tới cảm biến TMP36
+// chân analog kết nối tới cảm biến LM35
+float temp;
 void setup()
 {
   pinMode(12, OUTPUT);
-  Serial.begin(96000);
+  Serial.begin(9600);
   //không cần phải pinMode cho các chân analog trước khi dùng
 }
 
 void loop()
 {
   int x = analogRead(A0);
-  int t = map(x, 0 ,410 ,-50 , 150);
-  if(t>36)
-  digitalWrite(12, HIGH);
+  float voltage = x * 5.0 / 1024.0;
+  temp = voltage * 100.0;
+  Serial.println(temp);
+  if (temp > 36){
+    digitalWrite(12, HIGH);}
   else
-  digitalWrite(12, LOW);
+  {
+    digitalWrite(12, LOW);
+  }
   delay(1000); // Wait for 1000 millisecond(s)
 }
