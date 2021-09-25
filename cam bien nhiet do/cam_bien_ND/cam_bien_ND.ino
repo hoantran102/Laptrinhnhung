17881b3a5a89f7fbbd605a4cfc888926c98d4e9c
hoantran102 <hoanbem1234@gmail.com>
2021-09-25 10:09:42 +0700

    Create cam_bien_ND.ino

---
+++ cam bien nhiet do/cam_bien_ND/cam_bien_ND.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  pinMode(12, OUTPUT);
+  Serial.begin(96000);
+}
+
+void loop()
+{
+  int x = analogRead(A0);
+    int t = map(x, 0 ,410 ,-50 , 150);
+  if(t>36)
+  digitalWrite(12, HIGH);
+  else
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
