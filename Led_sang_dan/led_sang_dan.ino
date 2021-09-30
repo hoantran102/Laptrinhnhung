98493ff88e10e3f9cda761cfe033e0c8842e9156
hoantran102 <hoanbem1234@gmail.com>
2021-09-30 16:34:20 +0700

    Create led_sang_dan.ino

---
+++ Led_sang_dan/led_sang_dan.ino
@@ -0,0 +1,17 @@
+int brightness = 0;
+void setup()
+{
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  for (brightness = 0; brightness <=255 ; brightness +=5){
+    analogWrite(11,brightness);
+    delay(30);
+  }
+  for (brightness = 255; brightness <=0 ; brightness -=5) {  
+  analogWrite(11,brightness);
+    delay(30);
+}
+}
