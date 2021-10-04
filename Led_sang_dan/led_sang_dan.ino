becc941664bc204ec49f061485c2e4f85ce90e5b
hoantran102 <hoanbem1234@gmail.com>
2021-10-04 18:59:05 +0700

    Update led_sang_dan.ino

--- Led_sang_dan/led_sang_dan.ino
+++ Led_sang_dan/led_sang_dan.ino
@@ -1,17 +1,18 @@
 int brightness = 0;
+int LED=11;
 void setup()
 {
-  pinMode(11, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
   for (brightness = 0; brightness <=255 ; brightness +=5){
-    analogWrite(11,brightness);
+    analogWrite(LED,brightness);
     delay(30);
   }
   for (brightness = 255; brightness <=0 ; brightness -=5) {  
-  analogWrite(11,brightness);
+  analogWrite(LED,brightness);
     delay(30);
 }
 }
