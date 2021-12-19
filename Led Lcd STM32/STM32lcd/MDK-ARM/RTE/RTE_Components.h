dd4fc57d7eb22c8f488f81fa40917d4b6907eebb
hoantran102 <hoanbem1234@gmail.com>
2021-12-19 13:24:27 +0700

    bo sung

---
+++ Led Lcd STM32/STM32lcd/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32lcd' 
+ * Target:  'STM32lcd' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
